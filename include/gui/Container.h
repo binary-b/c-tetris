ae4485e4fbd65b5a4d997fc03d5d9c2c60d8044a
Binary Brain <binary@arbuz.localdomain>
2018-02-16 20:44:16 +0100

    Add README file
    Other changes:
      - implovements on some header files - multiple
        descriptions added

--- include/gui/Container.h
+++ include/gui/Container.h
@@ -1,6 +1,11 @@
 #ifndef _CONTAINER_H_
 #define _CONTAINER_H_
 
+/*
+ * contains another objects,
+ * objects should not touch each others
+ */
+
 extern void *Container;
 
 void cont_addView ( void *self, void *view );
